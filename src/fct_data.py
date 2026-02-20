f417eff5240f6b75a73616ec3aed3f188ae542c4
xdalp <mgarbe744@gmail.com>
2026-02-20 16:51:29 +0000

    Ajout de BoW IDF pour 1981

--- src/fct_data.py
+++ src/fct_data.py
@@ -1,7 +1,43 @@
 import zipfile
 from pathlib import Path
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import re
 
+#FONCTION MAIN 
+
+def raw_data_cleaning(path_meta,path_zip):
+    #lecture metdonnées
+    metadonnees = pd.read_csv("data/archelect_search.zip", compression="zip")
+
+    #decompression et creation dict.
+    texts = extract_texts_from_zips("data/text_files")
+
+    #merge avec métadonnées
+    meta_et_texts=meta_et_texts = pd.merge(
+        metadonnees,
+        texts,
+        on="id",
+        how="right")
+
+    #nettotyage stopwords
+    meta_et_texts['texte_clean'] = meta_et_texts['text'].apply(stopwords_cleaning)
+    meta_et_texts.drop_duplicates(inplace=True)
+
+    #format
+    meta_et_texts['annee'] = pd.to_datetime(meta_et_texts['date'], errors='coerce').dt.year
+
+    return meta_et_texts
+
+
+
+
+
+
+
+#SOUS-FONCTIONS 
+ 
 def extract_texts_from_zips(base_dir: str) -> pd.DataFrame:
     """
     Parcourt tous les fichiers ZIP dans `base_dir` (y compris les sous-dossiers),
@@ -39,3 +75,15 @@ def extract_texts_from_zips(base_dir: str) -> pd.DataFrame:
     df = pd.DataFrame(all_texts)
     print(f"Nombre total de documents extraits : {len(df)}")
     return df
+
+
+def stopwords_cleaning(text):
+    #télchargement des stopwords en français
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('french'))
+    text = str(text).lower()
+    text = re.sub(r'\d+', '', text)
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'[^\w\s]', '', text)
+    words = [w for w in text.split() if w not in stop_words]
+    return ' '.join(words)
